88e9fad311974bd765d2a55c09bb8c01ee4ea711
Shlok <shlokstha99@gmail.com>
2023-09-12 11:46:37 +0545

    added update feature

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/createBlog", (req, res) => {
 //CreateBlog Post
 app.post("/createBlog", async (req, res) => {
   const title = req.body.title;
-  const subTitle = req.body.subtitle;
+  const subTitle = req.body.subTitle;
   const description = req.body.description;
 
   //added data on database
@@ -56,6 +56,40 @@ app.get("/deleteBlog/:id", async (req, res) => {
 
   res.redirect("/");
 });
+//update form
+app.get("/editBlog/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const blog = await blogs.findAll({
+    where: {
+      id: id,
+    },
+  });
+
+  res.render("editBlog.ejs", { blog: blog });
+});
+
+app.post("/editBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  const subTitle = req.body.subTitle;
+  const description = req.body.description;
+  console.log(req.body)
+  await blogs.update(
+    {
+      title: title,
+      subTitle: subTitle,
+      description: description
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  res.redirect("/");
+});
+
 app.listen(3000, () => {
   console.log("project has started at 3000");
 });
